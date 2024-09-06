0a665a8b8485e134984e13519dc9388613e6649d
Hòa Kody <hoadangnguyen11@gmail.com>
2024-09-07 06:13:11 +0700

    edit database (1)

--- db/migrate/20240906114254-create-promontion.ts
+++ db/migrate/20240906114254-create-promontion.ts
@@ -3,7 +3,7 @@ import { QueryInterface, SequelizeStatic } from "sequelize";
 export default {
   up: (queryInterface: QueryInterface, Sequelize: SequelizeStatic) => {
     return queryInterface.createTable("promontions", {
-      promontionCode: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
